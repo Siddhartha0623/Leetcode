b6fe029fdae744c09786ae2fb30b12f0b0e0910b
Siddhartha0623 <chowdhurysiddhartha2006@gmail.com>
2026-09-25 23:26:41 +0530

    Time: 26 ms (31.2%), Space: 53.1 MB (6.4%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.java
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.java
@@ -1,72 +1,31 @@
 class Solution {
-    private String s;
-    private int pos;
+    TreeSet<String> ans = new TreeSet<>();
+
+    void dfs(String s) {
+        int r = s.indexOf('}');
+
+        // No braces left
+        if (r == -1) {
+            ans.add(s);
+            return;
+        }
+
+        // Find matching '{'
+        int l = s.lastIndexOf('{', r);
+
+        String left = s.substring(0, l);
+        String right = s.substring(r + 1);
+
+        // Content inside { }
+        String inside = s.substring(l + 1, r);
+
+        for (String part : inside.split(",")) {
+            dfs(left + part + right);
+        }
+    }
 
     public List<String> braceExpansionII(String expression) {
-        s = expression;
-        pos = 0;
-
-        Set<String> result = parseSeq();
-
-        List<String> ans = new ArrayList<>(result);
-        Collections.sort(ans);
-
-        return ans;
-    }
-
-    private Set<String> parseSeq() {
-        Set<String> result = new HashSet<>();
-        result.add("");
-
-        while (pos < s.length() &&
-               s.charAt(pos) != ',' &&
-               s.charAt(pos) != '}') {
-
-            Set<String> termSet = parseTerm();
-
-            Set<String> newResult = new HashSet<>();
-
-            for (String a : result) {
-                for (String b : termSet) {
-                    newResult.add(a + b);
-                }
-            }
-
-            result = newResult;
-        }
-
-        return result;
-    }
-
-    private Set<String> parseTerm() {
-        if (s.charAt(pos) == '{') {
-            pos++; // consume '{'
-
-            Set<String> union = new HashSet<>();
-
-            while (true) {
-                Set<String> seqSet = parseSeq();
-                union.addAll(seqSet);
-
-                if (s.charAt(pos) == ',') {
-                    pos++; // consume ','
-                } 
-                else if (s.charAt(pos) == '}') {
-                    pos++; // consume '}'
-                    break;
-                }
-            }
-
-            return union;
-
-        } else {
-            char c = s.charAt(pos);
-            pos++;
-
-            Set<String> singleton = new HashSet<>();
-            singleton.add(String.valueOf(c));
-
-            return singleton;
-        }
+        dfs(expression);
+        return new ArrayList<>(ans);
     }
 }
